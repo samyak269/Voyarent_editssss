30eac1af87e58c30bfb9676da15f313caadcdebd
siddhant <siddhantota@protonmail.com>
2023-12-01 17:14:04 +0530

    prisma

---
+++ tripfinder-envato-release/packages/boat/src/app/api/destinations/destination-carousel/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/prisma";
+
+export async function GET(req: NextRequest) {
+
+    try {
+
+        const destinations = await prisma.destinations.findMany()
+
+        if (!destinations) {
+            return new NextResponse(JSON.stringify({
+                success: false,
+                message: 'could not find destinations from the database'
+            }))
+        }
+
+        return new NextResponse(JSON.stringify({
+            success: true,
+            destinations: destinations
+        }))
+
+    } catch (error) {
+        return new NextResponse(JSON.stringify({
+            success: false,
+            error: error
+        }))
+    }
+
+}
